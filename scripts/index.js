d8ecc5a1f96002038d27d7bb6ae782bfe8eaabf2
Vadim Veshnyakov <veshvp00@gmail.com>
2025-06-16 20:58:13 +0300

    without webpack

--- scripts/index.js
+++ scripts/index.js
@@ -61,12 +61,30 @@ initialCards.forEach((item) => {
 
 // @todo: DOM узлы
 
+function closeByEsc(evt) {
+    if (evt.key === 'Escape') {
+        const openedPopup = document.querySelector('.popup_is-opened');
+        if (openedPopup) closeModal(openedPopup);
+    }
+}
+
+function closeByMouse(evt) {
+    const openedPopup = document.querySelector('.popup_is-opened');
+    if (evt.target.classList.contains('popup')) {
+        closeModal(openedPopup);
+    }
+}
+
 function openModal(popup) {      
     popup.classList.add('popup_is-opened');
+    document.addEventListener('keydown', closeByEsc)
+    document.addEventListener('mousedown', closeByMouse);
 }
 
 function closeModal(popup) {      
     popup.classList.remove('popup_is-opened');
+    document.removeEventListener('keydown', closeByEsc)
+    document.removeEventListener('mousedown', closeByMouse);
 }
 
 // @todo: Функция редактирования профиля
